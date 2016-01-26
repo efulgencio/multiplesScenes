3dd910cfd4aa48bb597571bb812bc1a8a6565072
eduardo fulgencio <eduardofulgencio@macbook-pro-de-eduardo.local>
2016-01-26 08:24:33 +0100

    Initial Commit

---
+++ MultipleScenes/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MultipleScenes
+//
+//  Created by eduardo fulgencio on 26/1/16.
+//  Copyright © 2016 eduardo fulgencio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MultipleScenes/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MultipleScenes
+//
+//  Created by eduardo fulgencio on 26/1/16.
+//  Copyright © 2016 eduardo fulgencio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
